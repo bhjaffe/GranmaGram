bf9c672174fd0110d1699fcd92822806423a84bd
Brian Jaffe <bhjaffe@gmail.com>
2015-01-30 18:17:45 -0800

    Added Bootstrap

--- granma/granma/urls.py
+++ granma/granma/urls.py
@@ -6,8 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', 'photoshare.views.home', name='home'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')),
-    # url(r'^/complete/google-oauth2/', 'photoshare.views.home', name='home'),
-    # url(r'^auth/google/', include('google_oauth.urls'))
 
     url(r'^admin/', include(admin.site.urls)),
 )
